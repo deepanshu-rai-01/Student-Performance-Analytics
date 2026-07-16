8bbec25980580ade898c42b202662b19a2b913d2
Deepanshu Rai <deepanshurai866@gmail.com>
2026-07-16 11:17:11 +0530

    Add Student Performance Analytics Project

---
+++ student_performance_analytics.py
@@ -0,0 +1,57 @@
+
+# total = 0
+# highest = 0
+# lowest = 100
+
+# highest_name = ""
+# lowest_name = ""
+
+# pass_count = 0
+
+# A = B = C = D = F = 0
+
+# for i in range(20):
+#     name = input("Enter Student Name: ")
+#     marks = int(input("Enter Marks: "))
+
+#     total += marks
+
+#     if marks > highest:
+#         highest = marks
+#         highest_name = name
+
+#     if marks < lowest:
+#         lowest = marks
+#         lowest_name = name
+
+#     if marks >= 40:
+#         pass_count += 1
+
+#     if marks >= 90:
+#         A += 1
+#     elif marks >= 75:
+#         B += 1
+#     elif marks >= 60:
+#         C += 1
+#     elif marks >= 40:
+#         D += 1
+#     else:
+#         F += 1
+
+# average = total / 20
+# pass_percentage = (pass_count / 20) * 100
+
+# print("\n------ Report ------")
+# print("Highest Scorer :", highest_name, highest)
+# print("Lowest Scorer :", lowest_name, lowest)
+# print("Average Marks :", average)
+# print("Pass Percentage :", pass_percentage, "%")
+
+# print("\nGrade Distribution")
+# print("A :", A)
+# print("B :", B)
+# print("C :", C)
+# print("D :", D)
+# print("F :", F)
+
+# print("\nTopper is", highest_name)
